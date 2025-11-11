2edaf78707aa4c04428a2bb062998efa25a718d0
Miroxun Anoraliyev <miroxunanaraliyev@gmail.com>
2025-11-11 21:10:38 +0500

    toliq

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home.jsx";
 import { MainLayout } from "./layout/main-layout.jsx";
 import { Auth } from "./pages/auth.jsx";
+import { Profile } from "./pages/profile.jsx";
+import { Contact } from './pages/contact.jsx';
 
 function App() {
   return (
-    <>
-     <Routes>
- 
-      <Route path="/auth" element={<Auth />} />
 
-      <Route path="/" element={<MainLayout />}>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/home" element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path="/home/about" element={<Profile />} />
+        <Route path="/home/contact" element={<Contact />} />
       </Route>
     </Routes>
-    </>
+
   );
 }
 
--- src/layout/main-layout.jsx
+++ src/layout/main-layout.jsx
@@ -1,6 +1,6 @@
-import { Outlet,Navigate } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-
+import { Link } from 'react-router-dom';
 export const MainLayout = () => {
     const token = Cookies.get("token");
     if (!token) {
@@ -9,7 +9,13 @@ export const MainLayout = () => {
 
     return (
         <>
-            <header>Header</header>
+            <header>
+                Header
+                <Link to={"/home"}>home</Link>
+                <Link to={"/home/contact"}>contact</Link>
+                <Link to={"/home/about"}>about</Link>
+
+            </header>
             <main>
                 <Outlet />
             </main>
